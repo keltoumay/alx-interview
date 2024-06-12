9e507343dde0c04d73f973fb172bb795243005bd
keltoumay <keltoumaouyahia@gmail.com>
2024-06-12 03:38:44 -0700

    matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
-"""
-def rotate_2d_matrix(matrix):
-    n = len(matrix)
+"""Rotates 2-D matrix:"""
 
-# Step 1: Transpose the matrix
-    for i in range(n):
-        for j in range(i, n):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-# Step 2: Reverse each row
-    for i in range(n):
-        matrix[i].reverse()
+def rotate_2d_matrix(m):
+    """Rotates in-place"""
+    n = len(m)
+    temp1, temp2 = 0, 0
 
+    for j in range(0, len(m) // 2 + 1):
+        for i in range(j, n - 1):
+            # For r in first row, put in same position in col from back
+            temp1 = m[i][n - 1]
+            m[i][n - 1] = m[j][i]
+            # put that temp1 in reverse position in row from bottom
+            temp2 = m[n - 1][n - 1 - i + j]
+            m[n - 1][n - 1 - i + j] = temp1
+            # put that temp2 in same position in col from front
+            temp1 = m[n - 1 - i + j][j]
+            m[n - 1 - i + j][j] = temp2
+            # put that temp1 in reverse position in row from top
+            m[j][i] = temp1
+        n -= 1
